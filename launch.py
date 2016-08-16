88e0613ac56a816f964cca180c2e3dac207ad25f
dimas777 <kriptexm@gmail.com>
2016-08-16 15:30:22 +0300

    rename value remove not needed line

--- launch.py
+++ launch.py
@@ -3,17 +3,19 @@ import configparser,  subprocess
 # parse and get serial number and serverIP from launcher.ini
 config = configparser.ConfigParser()
 config.read('launcher.ini')
-read1 = config.get('Options', 'Serial')
-read2 = config.get('Options', 'ServerIP')
+serialValue = config.get('Options', 'Serial')
+serverIPValue = config.get('Options', 'ServerIP')
+
 # parse and get serial number and serverIP from launcher.original.ini
-config = configparser.ConfigParser()
 config.read('launcher.original.ini')
-read3 = config.get('Options', 'Serial')
-read4 = config.get('Options', 'ServerIP')
+serialValueOriginal = config.get('Options', 'Serial')
+serverIPValueOriginal = config.get('Options', 'ServerIP')
+
 # compare values
-if read1 != read3:
-   read2 != read4;
+if serialValue != serialValueOriginal:
+   serverIPValue != serverIPValueOriginal;
    pass
+
 # copy launcher.original.in in launcher.ini
 read_orig_ini=open("launcher.original.ini", 'r')
 try:
@@ -25,15 +27,17 @@ try:
            write_orig_ini.close()
 finally:
       read_orig_ini.close()
+
 # check AutoLogin, Logins, Serial value
-config = configparser.ConfigParser()
 config.read('launcher.ini')
 Auto_login = config.get('Options', 'AutoLogin')
 Logins = config.get('Options', 'Logins')
 Serial = config.get('Options', 'Serial')
 if not Auto_login:
+
 # start gui
       subprocess.call("/home/dima/PycharmProjects/Python/gui.py", shell=True)
+
 # start gui2
 else:
     if not Logins:
