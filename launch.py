ca5aaccb42f0498d7aa8ef6109793626dfcdd54e
dimas777 <kriptexm@gmail.com>
2016-09-08 15:33:11 +0300

    correct logic

--- launch.py
+++ launch.py
@@ -1,4 +1,4 @@
-import configparser,  subprocess
+import configparser, subprocess
 
 # parse and get serial number and serverIP from launcher.ini
 config = configparser.ConfigParser()
@@ -12,21 +12,21 @@ serialValueOriginal = config.get('Options', 'Serial')
 serverIPValueOriginal = config.get('Options', 'ServerIP')
 
 # compare values
-if serialValue != serialValueOriginal:
-   serverIPValue != serverIPValueOriginal;
-   pass
+if  serialValue != serialValueOriginal:
+    serverIPValue != serverIPValueOriginal;
+    pass
 
 # copy launcher.original.in in launcher.ini
-read_orig_ini=open("launcher.original.ini", 'r')
+read_orig_ini = open("launcher.original.ini", 'r')
 try:
-      reading_file=read_orig_ini.read()
-      write_orig_ini=open("launcher.ini", 'w')
-      try:
-           write_orig_ini.write(reading_file)
-      finally:
-           write_orig_ini.close()
+    reading_file = read_orig_ini.read()
+    write_orig_ini = open("launcher.ini", 'w')
+    try:
+        write_orig_ini.write(reading_file)
+    finally:
+        write_orig_ini.close()
 finally:
-      read_orig_ini.close()
+    read_orig_ini.close()
 
 # check AutoLogin, Logins, Serial value
 config.read('launcher.ini')
@@ -34,14 +34,13 @@ Auto_login = config.get('Options', 'AutoLogin')
 Logins = config.get('Options', 'Logins')
 Serial = config.get('Options', 'Serial')
 if not Auto_login:
-
-# start gui
-      subprocess.call("/home/dima/PycharmProjects/Python/custom_login_gui.py", shell=True)
-
-# start gui2
+    if Logins and Serial:
+        # start gui2
+        subprocess.call("/home/dima/PycharmProjects/Python/custom_login_gui.py", shell=True)
+        subprocess.call("/home/dima/PycharmProjects/Python/start.py", shell=True)
+    else:
+        # start gui
+        subprocess.call("/home/dima/PycharmProjects/Python/error_gui.py", shell=True)
 else:
-    if not Logins:
-      subprocess.call("/home/dima/PycharmProjects/Python/error_gui.py", shell=True)
-    if not Serial:
-      subprocess.call("/home/dima/PycharmProjects/Python/error_gui.py", shell=True)
+    subprocess.call("/home/dima/PycharmProjects/Python/start.py", shell=True)
 
